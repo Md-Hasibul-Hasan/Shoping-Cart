0721f85e3bfd2053a22dee59d857ec5ec16bfacd
Md Hasibul Hasan <hasib.hasan.th@gmail.com>
2026-03-19 23:45:39 +0600

    Fix Nav component link to point to the home page

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ const Nav = () => {
   return (
     <nav className="flex justify-center gap-4 bg-slate-200 ">
       {/* <Link to="/">Home</Link> {" | "} */}
-      <Link to="/shop">Shop</Link> {" | "} 
+      <Link to="/">Shop</Link> {" | "} 
       <Link to="/cart">Cart</Link> {" | "}
       <Link to="/add">Add Product</Link> 
       <hr />
